27fcd9dfbf47cd8e2e0cca1eceb3c96a7498b2f5
hmcvay <hayleymcvay8@gmail.com>
2022-04-15 14:13:40 -0700

    WIP: re-factor prompt2.

--- prompt2/notes.js
+++ prompt2/notes.js
@@ -1,4 +1,16 @@
 
 let nums = [7,9,"hi",12,"hi", 7, 53];
-let uniqueNums = [...new Set(nums)];
+// let uniqueNums = [...new Set(nums)];
 
+
+function removeDup (nums) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] !== nums[j]){
+        result.push(nums[j]);
+      }
+    }
+  }
+  return result;
+}
