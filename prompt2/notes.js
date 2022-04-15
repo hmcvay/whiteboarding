7a109dd476b1a6d28e273e1bad99f023df119e20
hmcvay <hayleymcvay8@gmail.com>
2022-04-15 12:13:04 -0700

    WIP: prompt4.

--- prompt2/notes.js
+++ prompt2/notes.js
@@ -1,13 +1,4 @@
-let result = [];
-let nums = [7,9,"hi",12,"hi", 7, 53];
 
-function arrayDedup(nums) {
-  for (let i = 0; i< nums.length; i++) {
-    let x = nums.length-1;
-    if (nums[i] !== nums[x]){
-       result.push(nums[i])
-       x--;
-    }
-  }
-  return result;   
-}
+let nums = [7,9,"hi",12,"hi", 7, 53];
+let uniqueNums = [...new Set(nums)];
+
---
+++ prompt4/notes.js
@@ -0,0 +1,17 @@
+function unique(input) {
+  let x = input.length -1;
+  let i=0;
+  while(i<input.length){
+
+    while(i<x) {
+      if(i !== x) {
+        x--
+      }
+      else {
+        return false
+      }
+    }
+    
+  }
+}
+  
